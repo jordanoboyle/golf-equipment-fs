f0bd70b178e97e55b16b50ab753e3830ad08e0bf
jordanoboyle <jmcmcode@gmail.com>
2024-06-24 21:18:03 -0600

    show action, hardcode, two different types of buttons

--- app/controllers/clubs_controller.rb
+++ app/controllers/clubs_controller.rb
@@ -1,6 +1,24 @@
 class ClubsController < ApplicationController
 
   def index
+    @clubs = Club.all
     render :index
+  end
+
+  def show
+    @club = Club.find_by(id: params[:id])
+    render :show
+  end
+
+  def create
+    render :show    
+  end
+
+  def update
+    render :show
+  end
+
+  def delete
+    render :show
   end
 end
